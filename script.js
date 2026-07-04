5befb51f2ffa195ceb9d814f3bdbfabbba069afc
Rishav <147969248+rishwebb@users.noreply.github.com>
2026-07-05 01:59:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -294,7 +294,7 @@ const UI = {
                 <input type="number" value="${p.amount}" class="p-amount" data-idx="${idx}" min="0">
                 <select class="p-month" data-idx="${idx}">${mOpts}</select>
                 <button class="btn-icon text-danger p-del" data-idx="${idx}">
-                    <svg width="18" height="18" stroke="#ff453a" stroke-width="2" fill="none"><path d="M3 6h18M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+                    <svg width="18" height="18" viewBox="0 0 24 24" stroke="#ff453a" stroke-width="2" fill="none"><path d="M3 6h18M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
                 </button>
             `;
             DOM.projectList.appendChild(div);
